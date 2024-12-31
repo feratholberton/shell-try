9b64e4ea08609c622efea27c981bed3ff754f6ab
Fer <fer.falcon.dg@gmail.com>
2024-12-31 07:53:21 -0300

    Include stdlib to support EXIT_SUCCESS/FAILURE

--- utils/fgets_func.c
+++ utils/fgets_func.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 int main()
@@ -8,15 +9,14 @@ int main()
 	/** Create space for a string of 256 characters */
 	char input[maxChars];
 
-	while(true)
-	{
-		/** Print the prompt */
-		printf("Type: ");
+	/** Print the prompt */
+	printf("Type: ");
 
-		/** Get the user input and store it into the input var */
-		fgets(input, maxChars, stdin);
+	/** Get the user input and store it into the input var */
+	fgets(input, maxChars, stdin);
 
-		/** Print the input var */
-		printf("You typed: %s\n", input);
-	}
+	/** Print the input var */
+	printf("You typed: %s\n", input);
+
+	return (EXIT_SUCCESS);
 }
