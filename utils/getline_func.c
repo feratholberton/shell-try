199dec8db06f9d41229ba83c2a1b794a7d2687e9
Fer <fer.falcon.dg@gmail.com>
2024-12-31 09:31:06 -0300

    Basic 1 use getline function

--- utils/getline_func.c
+++ utils/getline_func.c
@@ -5,17 +5,17 @@ int main()
 {
 	/** If *lineptr (input) is set to NULL before the call, then getline() will allocate a buffer for storing the line. This buffer should be freed by the user program even if getline() failed. */
 	char *input = NULL;
-	size_t size;
+	size_t size, currentSize;
 
 	/** Print the prompt */
 	printf("Type, hit Enter and I'll get the line: ");
 
 	/** Get the user input and store it in input var */
-	getline(&input, &size, stdin);
+	currentSize = getline(&input, &size, stdin);
 
 	/** Print the input var */
 	printf("You typed: %s", input);
-	printf("%zd characters\n", size);
+	printf("%ld chars in total\n", currentSize);
 
 	free(input);
 	return (EXIT_SUCCESS);
