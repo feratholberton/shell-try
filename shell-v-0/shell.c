57c4b575bf0ef38807d098d9d0993d06aaac4790
Fer <fer.falcon.dg@gmail.com>
2024-12-31 09:56:47 -0300

    PRE - Shell init

---
+++ shell-v-0/shell.c
@@ -0,0 +1,6 @@
+#include "shell.h"
+
+int main(void) {
+	printf("This will be a very simple shell\n");
+	return (EXIT_SUCCESS);
+}
---
+++ shell-v-0/shell.h
@@ -0,0 +1,10 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#endif
