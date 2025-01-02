94b776592ca232c94a1a94353c50415c76fb508b
Fer <fer.falcon.dg@gmail.com>
2025-01-02 19:42:52 -0300

    _strtok memory management

--- shell-v-0/_getline.c
+++ shell-v-0/_getline.c
@@ -5,19 +5,26 @@ char *_getline()
 	/** If *lineptr => (input) is set to NULL before the call, then getline() will allocate a buffer for storing the line. This buffer should be freed by the user program even if getline() failed. */
 	char *input = NULL;
 	size_t size = 0;
-	size_t currentSize;
+	ssize_t length;
 
 	/** Get the user input and store it in input var */
-	currentSize = getline(&input, &size, stdin);
+	length = getline(&input, &size, stdin);
+	if (length == -1)
+	{
+		free(input);
+		return NULL;
+	}
 
+	if (input == NULL)
+		return NULL;
 	/* Replace new line with null terminator */
 	input[strcspn(input, "\n")] = '\0';
 
-	/** Print the input var */
-	printf("You typed: %s\n", input);
+	/** Print the input var
+	printf("You typed: %s\n", input); */
 
-	/** Print chars processed */
-	printf("%ld total chars including '\\n'\n", currentSize);
+	/** Print chars processed 
+	printf("%ld total chars including '\\n'\n", currentSize);*/
 
 	return input;
 }
--- shell-v-0/_strtok.c
+++ shell-v-0/_strtok.c
@@ -1,12 +1,19 @@
 #include "shell.h"
 
-char **_strtok(char *string, char *delimeter, int *token_count) 
+char **_strtok(char *string, char *delimeter, int *token_count)
 {
 	char *tokens_str = strdup(string);
 	char **tokens = NULL;
 	char *token;
 	char **new_tokens;
 	int count = 0;
+	int i;
+
+	if (tokens_str == NULL)
+	{
+		perror("strdup() FAILED");
+		return NULL;
+	}
 
 	token = strtok(tokens_str, delimeter);
 
@@ -16,16 +23,39 @@ char **_strtok(char *string, char *delimeter, int *token_count)
 		if (new_tokens == NULL)
 		{
 			perror("realloc() FAILED");
-			free(tokens);
 			free(tokens_str);
+
+			for (i = 0; i < count; i++)
+			{
+				free(tokens[i]);
+			}
+			free(tokens);
+
 			return NULL;
 		}
 
 		tokens = new_tokens;
-		tokens[count++] = strdup(token);
+		tokens[count] = strdup(token);
+		if (tokens[count] == NULL)
+		{
+			perror("strdup() FAILED");
 
+			free(tokens_str);
+
+			for (i = 0; i < count; i++)
+			{
+				free(tokens[i]);
+			}
+			free(tokens);
+
+			return NULL;
+		}
+
+		count++;
 		token = strtok(NULL, delimeter);
 	}
+
+	free(tokens_str);
 
 	if (token_count)
 	{
