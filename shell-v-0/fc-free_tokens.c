bba81d55fb1ee54c3c5cd4cb11ead5317ae3998d
Fer <fer.falcon.dg@gmail.com>
2025-01-03 08:54:10 -0300

    Check for tokens

--- shell-v-0/fc-free_tokens.c
+++ shell-v-0/fc-free_tokens.c
@@ -4,9 +4,18 @@ void free_tokens(char **tokens, int count)
 {
 	int i;
 
+	if (tokens == NULL || count < 0)
+	{
+		fprintf(stderr, "free_tokens: Invalid tokens or count\n");
+		return;
+	}
+
 	for (i = 0; i < count; i++)
 	{
-		free(tokens[i]);
+		if (tokens[i] != NULL)
+		{
+			free(tokens[i]);
+		}
 	}
 
 	free(tokens);
