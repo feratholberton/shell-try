92bd5362b6d4e84267160e76ed8295a1151a3288
Fer <fer.falcon.dg@gmail.com>
2025-01-03 08:53:48 -0300

    fc_get_tokens changes to strtok_r

--- shell-v-0/fc_get_tokens.c
+++ shell-v-0/fc_get_tokens.c
@@ -2,53 +2,45 @@
 
 char **get_tokens(char *string, char *delimeter, int *token_count)
 {
-	char *tokens_str = strdup(string);
+	char *tokens_str;
 	char **tokens = NULL;
 	char *token;
+	char *save_ptr;
 	char **new_tokens;
 	int count = 0;
-	int i;
 
+	tokens_str = strdup(string);
 	if (tokens_str == NULL)
 	{
+		perror("strdup() FAILED");
 		return NULL;
 	}
 
-	token = strtok(tokens_str, delimeter);
-
+	token = strtok_r(tokens_str, delimeter, &save_ptr);
 	while (token != NULL)
 	{
 		new_tokens = realloc(tokens, (count + 1) * sizeof(char *));
 		if (new_tokens == NULL)
 		{
+			perror("realloc() FAILED");
 			free(tokens_str);
-
-			for (i = 0; i < count; i++)
-			{
-				free(tokens[i]);
-			}
-			free(tokens);
-
+			free_tokens(tokens, count);
 			return NULL;
 		}
 
 		tokens = new_tokens;
+
 		tokens[count] = strdup(token);
 		if (tokens[count] == NULL)
 		{
+			perror("strdup() FAILED");
 			free(tokens_str);
-
-			for (i = 0; i < count; i++)
-			{
-				free(tokens[i]);
-			}
-			free(tokens);
-
+			free_tokens(tokens, count);
 			return NULL;
 		}
 
 		count++;
-		token = strtok(NULL, delimeter);
+		token = strtok_r(NULL, delimeter, &save_ptr);
 	}
 
 	free(tokens_str);
@@ -56,6 +48,12 @@ char **get_tokens(char *string, char *delimeter, int *token_count)
 	if (token_count)
 	{
 		*token_count = count;
+	}
+
+	if (count == 0)
+	{
+		free(tokens);
+		tokens = NULL;
 	}
 
 	return tokens;
