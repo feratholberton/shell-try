4da6b823710399cb84d27bc2cf87df569b3fe9fc
Fer <fer.falcon.dg@gmail.com>
2025-01-03 01:46:57 -0300

    get_line checks

--- shell-v-0/fc_get_line.c
+++ shell-v-0/fc_get_line.c
@@ -9,22 +9,21 @@ char *get_line()
 
 	/** Get the user input and store it in input var */
 	length = getline(&input, &size, stdin);
+
 	if (length == -1)
 	{
 		free(input);
 		return NULL;
 	}
 
-	if (input == NULL)
+	if (length == 1)
+	{
+		free(input);
 		return NULL;
+	}	
+
 	/* Replace new line with null terminator */
 	input[strcspn(input, "\n")] = '\0';
-
-	/** Print the input var
-	printf("You typed: %s\n", input); */
-
-	/** Print chars processed 
-	printf("%ld total chars including '\\n'\n", currentSize);*/
 
 	return input;
 }
